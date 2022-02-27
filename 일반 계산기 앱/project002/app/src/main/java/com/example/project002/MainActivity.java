02a92eeb756b83ac16003bb4778ae8e3933a2d65
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-02-27 23:10:33 +0900

    결과 및 계산 텍스트뷰 동적 추가

--- 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
+++ 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.project002;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.project002.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isFirstInput = true;
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
     }
+
+
+    public void num1Click (View view){
+        //activityMainBinding.resultTxt.setVisibility(View.GONE);
+        activityMainBinding.resultTxt.append("1");
+        activityMainBinding.mathTxt.append("1");
+    }
+
+
+
 }
