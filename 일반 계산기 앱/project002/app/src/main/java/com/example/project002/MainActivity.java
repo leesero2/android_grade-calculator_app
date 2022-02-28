e0876930ab6166274606a8e59cf48d5628ca4bef
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-02-28 18:20:17 +0900

    tag함수를 이용해 간략하게 정리

--- 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
+++ 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
@@ -21,99 +21,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void numButtonClick(View view){
-
-    }
-
-
-    public void num1Click(View view) {
-        Button button = findViewById(view.getId());
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("1");
-            activityMainBinding.mathTxt.setText("1");
-        } else {
-            activityMainBinding.resultTxt.append("1");
-            activityMainBinding.mathTxt.append("1");
-        }
-        //activityMainBinding.resultTxt.setVisibility(View.GONE);
-    }
-
-    public void num2Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("2");
-            activityMainBinding.mathTxt.setText("2");
-        } else {
-            activityMainBinding.resultTxt.append("2");
-            activityMainBinding.mathTxt.append("2");
+        String getButtonText = view.getTag().toString(); //xml 디자인에 tag값을 넣어주면 따로 findid값을 지정안해도 getTag()를 통해 사용가능
+        if(isFirstInput){
+            //activityMainBinding.resultTxt.setText(view.getTag().toString()); 처럼 사용가능
+            activityMainBinding.resultTxt.setText(getButtonText);
+            activityMainBinding.mathTxt.setText(getButtonText);
+            isFirstInput = false;
+        }else{
+            activityMainBinding.resultTxt.append(getButtonText);
+            activityMainBinding.mathTxt.append(getButtonText);
         }
     }
 
-    public void num3Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("3");
-            activityMainBinding.mathTxt.setText("3");
-        } else {
-            activityMainBinding.resultTxt.append("3");
-            activityMainBinding.mathTxt.append("3");
-        }
-    }
-
-    public void num4Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("4");
-            activityMainBinding.mathTxt.setText("4");
-        } else {
-            activityMainBinding.resultTxt.append("4");
-            activityMainBinding.mathTxt.append("4");
-        }
-    }
-
-    public void num5Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("5");
-            activityMainBinding.mathTxt.setText("5");
-        } else {
-            activityMainBinding.resultTxt.append("5");
-            activityMainBinding.mathTxt.append("5");
-        }
-    }
-
-    public void num6Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("6");
-            activityMainBinding.mathTxt.setText("6");
-        } else {
-            activityMainBinding.resultTxt.append("6");
-            activityMainBinding.mathTxt.append("6");
-        }
-    }
-
-    public void num7Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("7");
-            activityMainBinding.mathTxt.setText("7");
-        } else {
-            activityMainBinding.resultTxt.append("7");
-            activityMainBinding.mathTxt.append("7");
-        }
-    }
-
-    public void num8Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("8");
-            activityMainBinding.mathTxt.setText("8");
-        } else {
-            activityMainBinding.resultTxt.append("8");
-            activityMainBinding.mathTxt.append("8");
-        }
-    }
-
-    public void num9Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTxt.setText("9");
-            activityMainBinding.mathTxt.setText("9");
-        } else {
-            activityMainBinding.resultTxt.append("9");
-            activityMainBinding.mathTxt.append("9");
-        }
-    }
 }
