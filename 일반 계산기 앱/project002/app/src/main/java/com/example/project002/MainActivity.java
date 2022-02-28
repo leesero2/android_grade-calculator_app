4692305462af714ad07c5a3fa9153ac4998ebf74
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-02-28 22:43:07 +0900

    뒤로가기 버튼 구현

--- 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
+++ 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project002;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -32,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
             //activityMainBinding.resultTxt.setText(view.getTag().toString()); 처럼 사용가능
             activityMainBinding.resultTxt.setText(getButtonText);
             isFirstInput = false;
+            if(operator.equals("=")){
+                activityMainBinding.mathTxt.setText(null);
+                isOperatorClick = false;
+            }
         }else{
             if(activityMainBinding.resultTxt.getText().toString().equals("0")){
                 Toast.makeText(this, "0으로 시작되는 숫자는 없습니다.", Toast.LENGTH_LONG).show();
@@ -48,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         resultNumber = 0;
         operator = "+";
         isFirstInput = true;
+        isOperatorClick = false;
     }
 
     public void pointButtonClick (View view){
@@ -65,15 +71,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void operatorClick (View view){
         isOperatorClick = true;
+        lastOperator = view.getTag().toString();
+        if(isFirstInput){
+            if(operator.equals("=")){
+                operator = view.getTag().toString();
+                resultNumber = Double.parseDouble(activityMainBinding.resultTxt.toString());
+                activityMainBinding.mathTxt.setText(resultNumber + " " + operator + " ");
+            }else{
+                operator = view.getTag().toString();
+                String getMathTxt = activityMainBinding.mathTxt.getText().toString();
+                String subString = getMathTxt.substring(0, getMathTxt.length() - 2);
+                activityMainBinding.mathTxt.setText(subString);
+                activityMainBinding.mathTxt.append(operator + " ");
+            }
 
-        inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString());
+        }else{
+            inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString());
+            resultNumber = calculator(resultNumber, inputNumber, operator);
 
-        resultNumber = calculator(resultNumber, inputNumber, operator);
+            activityMainBinding.resultTxt.setText(String.valueOf(resultNumber));
+            isFirstInput = true;
+            operator = view.getTag().toString();
+            activityMainBinding.mathTxt.append(inputNumber + " " + operator + " ");
 
-        activityMainBinding.resultTxt.setText(String.valueOf(resultNumber));
-        isFirstInput = true;
-        operator = view.getTag().toString();
-        activityMainBinding.mathTxt.append(inputNumber + " " + operator + " ");
+        }
     }
 
     public void equalsButtonClick (View view){
@@ -95,6 +116,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void backspaceButtonClick(View view){
+        if(!isFirstInput){
+            String getResultTxt = activityMainBinding.resultTxt.getText().toString();
+            if(getResultTxt.length() > 0) {
+                String subString = getResultTxt.substring(0, getResultTxt.length() - 1);
+                activityMainBinding.resultTxt.setText(subString);
+            }else{
+                activityMainBinding.resultTxt.setText("0");
+                isFirstInput = true;
+            }
+        }
+    }
+
     private double calculator(double resultNumber, double inputNumber, String operator) {
         switch (operator){
             case "=":
@@ -111,6 +145,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case "÷":
                 resultNumber = resultNumber / inputNumber;
+                break;
+            default:
+                Log.e("calculator", resultNumber + " " + inputNumber + " " + operator);
                 break;
         }
         return resultNumber;
