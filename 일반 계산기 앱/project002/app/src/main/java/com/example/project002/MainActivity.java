04aec7a2665dc2aaf9bc1a9b5243b4ad92f66568
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-02-28 18:44:25 +0900

    초기화 버튼 구현

--- 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
+++ 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.project002.databinding.ActivityMainBinding;
 
@@ -29,13 +30,25 @@ public class MainActivity extends AppCompatActivity {
         if(isFirstInput){
             //activityMainBinding.resultTxt.setText(view.getTag().toString()); 처럼 사용가능
             activityMainBinding.resultTxt.setText(getButtonText);
-            //activityMainBinding.mathTxt.setText(getButtonText);
             isFirstInput = false;
         }else{
-            activityMainBinding.resultTxt.append(getButtonText);
-            //activityMainBinding.mathTxt.append(getButtonText);
+            if(activityMainBinding.resultTxt.getText().toString().equals("0")){
+                Toast.makeText(this, "0으로 시작되는 숫자는 없습니다.", Toast.LENGTH_LONG).show();
+                isFirstInput = true;
+            }else{
+                activityMainBinding.resultTxt.append(view.getTag().toString());
+            }
         }
     }
+
+    public void allClearButtonClick (View view){
+        activityMainBinding.resultTxt.setText("0");
+        activityMainBinding.mathTxt.setText("");
+        resultNumber = 0;
+        operator = "+";
+    }
+
+
 
     public void operatorClick (View view){
         double inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString());
