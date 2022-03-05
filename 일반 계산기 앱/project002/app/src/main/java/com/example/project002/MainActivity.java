7eb1dadac6da6a660950c53a3ec70b9da5c0b979
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-03-05 17:14:22 +0900

    수정 및 주석 추가

--- 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
+++ 일반 계산기 앱/project002/app/src/main/java/com/example/project002/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
     boolean isFirstInput = true; //계산기에선 이 버튼이 첫번째로 입력한 숫자냐 아니냐를 확인하기위해 boolean으로 변수 isFirstInput 생성
     boolean isOperatorClick = false; //연산자가 눌렀는지 안눌렀는지 체크하는 변수 생성
     double resultNumber = 0; //결과값을 저장할 변수 생성
-    double inputNumber = 0; //전역변수로 선언 (마지막에 사용된 값을 알수없기 때문에)
+    double inputNumber = 0; //전역변수로 선언 (메서드를 탈출하면 마지막에 사용된 값을 알수없기 때문에)
     String operator = "="; //연산자를 저장할 변수 생성
-    String lastOperator = "+"; //마지막 연산자 변수 생성
+    String lastOperator = "⁺"; //마지막 연산자를 저장할 변수 생성
 
     ActivityMainBinding activityMainBinding; //뷰바인딩 선언
 
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         }else{ //처음 입력한게 아니라면 ( 이미 숫자를 미리 눌렀다면 )
             if(activityMainBinding.resultTxt.getText().toString().contains(".")){ //contains함수를 통해 소수점이 이미 존재한다면 ( 처음 누른것도 소수점이고 또 소수점을 눌렀을때의 조건 )
                 Toast.makeText(this, "이미 소숫점이 존재합니다.",Toast.LENGTH_LONG).show(); //이미 소숫점이 존재한다는 토스트 발생
-            }else{
+            }else{ // 처음 입력한 값이 존재하지만 소숫점이 아니라면
                 activityMainBinding.resultTxt.append(view.getTag().toString()); //아니라면 resulrTxt에 소숫점을 붙임( 슷자를 먼저 눌렀다는 말이니까 그뒤에 소숫점을 붙임)
             }
         }
@@ -78,45 +78,45 @@ public class MainActivity extends AppCompatActivity {
     public void operatorClick (View view){
         isOperatorClick = true; //연산자를 눌렀으니 true로 초기화
         lastOperator = view.getTag().toString(); //마지막에 입력된 연산자를 갖고옴
-        if(isFirstInput){
+        if(isFirstInput){ //처음 입력한게 맞다면
             if(operator.equals("=")){ //연산자 비교를 통해 참이면
                 operator = view.getTag().toString(); //지금 누른 연산자를 저장
                 resultNumber = Double.parseDouble(activityMainBinding.resultTxt.toString());
                 activityMainBinding.mathTxt.setText(resultNumber + " " + operator + " ");
             }else{
                 operator = view.getTag().toString(); //지금 누른 연산자를 저장
-                String getMathTxt = activityMainBinding.mathTxt.getText().toString();
-                String subString = getMathTxt.substring(0, getMathTxt.length() - 2); //0번부터 getMathTxt 길이에서 2개를 삭제
-                activityMainBinding.mathTxt.setText(subString);
-                activityMainBinding.mathTxt.append(operator + " ");
+                String getMathTxt = activityMainBinding.mathTxt.getText().toString(); //mathTxt에 있는 문자열을 저장을 함
+                String subString = getMathTxt.substring(0, getMathTxt.length() - 2); //0번부터 getMathTxt 길이에 -2를 한 길이까지만 뽑아서 sub에 저장을 함
+                activityMainBinding.mathTxt.setText(subString); //그값을 mathTxt에 나타냄
+                activityMainBinding.mathTxt.append(operator + " "); //mathTxt에 operator를 append함
             }
 
         }else{
-            inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString()); //맨앞에 Double inputNumber... 로 지정하게되면 지정된 함수의 자료형은 double인데 결과를 받는데이터는 String이라 Double.parseDouble을 통해 문자열을 double형으로 변환하게끔 선언
+            inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString()); //맨앞에 Double inputNumber... 로 지정하게되면 지정된 함수의 자료형은 double인데 결과를 받는데이터는 String이라 Double.parseDouble을 통해 문자열을 double형으로 변환하게끔 선언 / input넘버를 꼭 전역변수로 설정해야 함(지역변수로 사용되면 해당 메소드에서 사용되고 나올때 값이 사라짐)
             resultNumber = calculator(resultNumber, inputNumber, operator); //받아온 연산 값을 resultNumber에 저장
-            activityMainBinding.resultTxt.setText(String.valueOf(resultNumber));
-            isFirstInput = true;
-            operator = view.getTag().toString();
-            activityMainBinding.mathTxt.append(inputNumber + " " + operator + " ");
+            activityMainBinding.resultTxt.setText(String.valueOf(resultNumber)); //결과 텍스트에 결과변수 데이터값을 표시
+            isFirstInput = true; //결과를 나타내었으니 처음 입력을 true로 초기화
+            operator = view.getTag().toString(); //마지막 연산자를 operator에 저장
+            activityMainBinding.mathTxt.append(inputNumber + " " + operator + " "); //math부분에 append로 표시
 
         }
     }
 
-    // '=' 메소드 (이걸 만든 이유는 일반 윈도우 계산기에선 =을 연달아 누르면 마지막에 했던 연산을 쭉 이어가기 때문에 예외처리를 함)
+    // '=' 메소드 (이걸 만든 이유는 일반 윈도우 계산기에선 '=' 을 연달아 누르면 마지막에 했던 연산을 쭉 이어가기 때문에 예외처리를 함)
     public void equalsButtonClick (View view){
-        if(isFirstInput){
-            if(isOperatorClick){
+        if(isFirstInput){ //처음 입력한 값이 맞다면
+            if(isOperatorClick){ //operatorclick 메서드에서 true로 초기화 되었으면 실행
                 activityMainBinding.mathTxt.setText(resultNumber + " " + lastOperator + " " + inputNumber + " ="); //마지막 결과, 마지막 연산, 인풋 넘버
                 resultNumber = calculator(resultNumber,inputNumber,lastOperator);
                 activityMainBinding.resultTxt.setText(String.valueOf(resultNumber));
             }
         }else {
-            inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString());
+            inputNumber = Double.parseDouble(activityMainBinding.resultTxt.getText().toString()); //input넘버를 꼭 전역변수로 설정해야 함(지역변수로 사용되면 해당 메소드에서 사용되고 나올때 값이 사라짐)
             resultNumber = calculator(resultNumber, inputNumber, operator); //받아온 연산 값을 resultNumber에 저장
             lastOperator = operator; //마지막 연산자를 lastOperator에 저장
             activityMainBinding.resultTxt.setText(String.valueOf(resultNumber)); //resultTxt에 결과 함수를 넣음
             isFirstInput = true; //결과가 끝났기에 true로 변경
-            operator = view.getTag().toString(); //현재 연산자를 operator에 저장
+            operator = view.getTag().toString(); //'=' 연산자를 operator에 저장
             activityMainBinding.mathTxt.append(inputNumber + " " + operator + " ");
         }
     }
@@ -144,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
             case "+":
                 resultNumber = resultNumber + inputNumber;
                 break;
-            case "_":
+            case "-":
                 resultNumber = resultNumber - inputNumber;
                 break;
             case "x":
@@ -153,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
             case "÷":
                 resultNumber = resultNumber / inputNumber;
                 break;
-            default:
+            default: //여기있는 문자가 없을때 default 값으로 이동
                 Log.e("calculator", resultNumber + " " + inputNumber + " " + operator);
                 break;
         }
